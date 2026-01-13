ae2409848c238b29ea2ade6e9dedf21e45b10722
V V N S POORNA CHANDRIKA <chandrikavvns31@gmail.com>
2026-01-13 14:33:48 +0530

    Refactor smoothie app to use Pandas DataFrame
    Updated the app to use Pandas for fruit options and improved data handling.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,59 +1,63 @@
 # Import python packages
 import requests
 import streamlit as st
+import pandas as pd
 from snowflake.snowpark.functions import col
 
 # Write directly to the app
 st.title("🥤 Customize Your Smoothie!")
-
 st.write("Choose the fruits you want in your custom Smoothie!")
 
-# Name input
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be:", name_on_order)
 
-# Snowflake connection
+# Connect to Snowflake
 cnx = st.connection("snowflake")
 session = cnx.session()
 
-# Get fruit names and search values
+# Get dataframe with SEARCH_ON
 my_dataframe = session.table("smoothies.public.fruit_options") \
     .select(col("FRUIT_NAME"), col("SEARCH_ON")) \
     .collect()
 
-fruit_dict = {row["FRUIT_NAME"]: row["SEARCH_ON"] for row in my_dataframe}
-fruit_list = list(fruit_dict.keys())
+# Convert Snowpark DF → Pandas DF
+pd_df = pd.DataFrame(my_dataframe)
 
-# Multiselect
+# Multiselect list
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
-    fruit_list,
+    pd_df["FRUIT_NAME"].tolist(),
     max_selections=5
 )
 
-# If fruits selected
+# When user selects fruits
 if ingredients_list:
-    ingredients_string = ''
+    ingredients_string = ""
 
     for fruit_chosen in ingredients_list:
-        ingredients_string += fruit_chosen + ' '
+
+        ingredients_string += fruit_chosen + " "
+
+        # 🔥 Get SEARCH_ON value using pandas
+        search_on = pd_df.loc[
+            pd_df["FRUIT_NAME"] == fruit_chosen,
+            "SEARCH_ON"
+        ].iloc[0]
+
+        st.write("The search value for ", fruit_chosen, " is ", search_on)
 
         st.subheader(fruit_chosen + " Nutrition Information")
 
-        search_value = fruit_dict[fruit_chosen]
-
-        smoothiefroot_response = requests.get(
-            "https://my.smoothiefroot.com/api/fruit/" + search_value.lower()
+        response = requests.get(
+            "https://my.smoothiefroot.com/api/fruit/" + search_on.lower()
         )
 
-        if smoothiefroot_response.status_code == 200:
-            st.dataframe(
-                data=smoothiefroot_response.json(),
-                use_container_width=True
-            )
+        if response.status_code == 200:
+            st.dataframe(response.json(), use_container_width=True)
         else:
             st.write("Sorry, that fruit is not in our database.")
 
+    # Insert statement
     my_insert_stmt = f"""
         insert into smoothies.public.orders(ingredients, name_on_order)
         values ('{ingredients_string}','{name_on_order}')
